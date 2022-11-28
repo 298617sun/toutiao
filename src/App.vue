7ebab0c1cb766845c5a9010479856af79301172f
smx <10401716+lib_smx@user.noreply.gitee.com>
2022-11-28 20:51:41 +0800

    引入vant

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <router-view/>
-    <h1>1213</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
